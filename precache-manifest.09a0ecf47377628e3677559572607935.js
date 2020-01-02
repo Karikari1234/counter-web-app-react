6f71ed06262097605ed0a0e15e7c94219cbad02f
nafiz <naafizrahman1@gmail.com>
2020-01-02 12:57:01 +0600

    Updates

---
+++ precache-manifest.09a0ecf47377628e3677559572607935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e394a9fe74222509f596c4ad67339389",
+    "url": "/counter-web-app-react/index.html"
+  },
+  {
+    "revision": "b8ec42a35b4909e4e3f0",
+    "url": "/counter-web-app-react/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "50c880d3fb1bf112b526",
+    "url": "/counter-web-app-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8ec42a35b4909e4e3f0",
+    "url": "/counter-web-app-react/static/js/2.e34c7a65.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter-web-app-react/static/js/2.e34c7a65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50c880d3fb1bf112b526",
+    "url": "/counter-web-app-react/static/js/main.ebb9e6d5.chunk.js"
+  },
+  {
+    "revision": "a74cb6cc3cc6c75eeebe",
+    "url": "/counter-web-app-react/static/js/runtime-main.faa7574a.js"
+  }
+]);
